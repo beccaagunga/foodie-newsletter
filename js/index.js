6c433e6ba694861b668b1d8b721f2a4d6c395d3d
Becca Agunga <beccaagunga@gmail.com>
2020-01-01 15:17:28 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// window.onload = function() {
+//     if (window.jQuery) {
+//         // jQuery is loaded
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
+
+// Get modal element
+const modal = document.getElementById('simpleModal');
+// Get modal button
+const modalBtn = document.getElementById('modalBtn');
+//Close
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+// Listen for outside click
+window.addEventListener('click', clickOutside);
+// Function to close for outside click
+function clickOutside(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
+// Listen for a open click on btn
+modalBtn.addEventListener('click', openModal);
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
